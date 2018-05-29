ba42d98c28e7b53db3909f0cb165c3a3993982a6
TestingKbiz <testing.kbiz@gmail.com>
2018-05-29 20:48:05 +0530

    laravel changes new12

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,6 +1,5 @@
 <?php
 class HomeController extends BaseController {
-
 	/*
 	|--------------------------------------------------------------------------
 	| Default Home Controller
@@ -13,14 +12,16 @@ class HomeController extends BaseController {
 	|	Route::get('/', 'HomeController@showWelcome');
 	|
 	*/
-
-	public function showWelcome()
-	{
-		return View::make('hello');
-	}
+	 public function __construct()
+    {		
+		$this->apikey='uoNHpMHDTRtgmP0jrRv2Hpsw4f22i3xB5RYlUxbMybkDm9cX2t9bSTUbiGbn2pik';
+		$this->apisec='hdAP9DPWR8dOP5qzitsFxiMw44tczgeWCNQeavlfN3jRoiD0CpCTxCXsw8BMdCJi';
+    }
 
 	public function binanceAgain(){
-		$api12 = new Binance\API("uoNHpMHDTRtgmP0jrRv2Hpsw4f22i3xB5RYlUxbMybkDm9cX2t9bSTUbiGbn2pik","hdAP9DPWR8dOP5qzitsFxiMw44tczgeWCNQeavlfN3jRoiD0CpCTxCXsw8BMdCJi");
+		$apik = $this->apikey;
+		$apisec = $this->apisec;
+		$api12 = new Binance\API($apik,$apisec);
 		$ticker = $api12->prices();
 		echo"<pre>";
 		print_r($ticker); // List prices of all symbols
